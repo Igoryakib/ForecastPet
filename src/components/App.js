6b2f9da065ed194611f563f10b26fbc240fe63aa
Igoryakib <Igoryakib@gmail.com>
2023-07-17 11:59:51 +0300

    added footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Footer from "./Footer/Footer";
+
 
 const  App = () => {
   return (
     <>
       <h1 className="App">Happy hacking ! :)</h1>
+      <Footer/>
     </>
   );
 }
