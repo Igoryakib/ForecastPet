d743f0e9d611cc4f7e70581d6720b0f11a5fbb24
Антон Соляник <solyanicks@gmail.com>
2023-07-21 02:22:20 +0200

    login page/added a close button + reroute functionality to it. --v2

--- src/components/Authentication/signup/form/Form.jsx
+++ src/components/Authentication/signup/form/Form.jsx
@@ -1,13 +1,18 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 import styles from './Form.module.scss';
 import FormHeader from './FormHeader';
 import Inputs from './Inputs';
 
 const Form = function(){
+  const navigate = useNavigate()
+
   return (
     <div className={styles.container}>
+      <button className={styles.closeButton} onClick={() => navigate('/')}>&times;</button>
       <FormHeader />
       <Inputs />
-      
+
     </div>
   )
 }
