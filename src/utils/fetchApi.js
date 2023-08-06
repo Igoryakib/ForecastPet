bf70391cc807792b74cbeecbaba85f4580bd346b
Igoryakib <Igoryakib@gmail.com>
2023-08-06 21:43:29 +0300

    added fetch functions

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,1 +1,36 @@
 import axios from "axios";
+
+axios.defaults.baseURL = "https://api.openweathermap.org/data/2.5";
+
+const API_KEY = "9ea4891c8392d9c904174b8d758cb626";
+
+export const getWeatherData = async (regionData, type, lang) => {
+  try {
+    const geoData = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${regionData}&limit=5&appid=${API_KEY}`
+    );
+    const { lat, lon } = geoData.data[0];
+    switch (type) {
+      case "hourly":
+        const hourlyWeather = await axios.get(
+          `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&lang=${lang}&appid=${API_KEY}`
+        );
+        return hourlyWeather.data;
+      case "currently":
+        const currentlyWeather = await axios.get(
+          `/weather?lat=${lat}&lon=${lon}&lang=${lang}&appid=${API_KEY}`
+        );
+        return currentlyWeather.data;
+      case "daily":
+        const dailyWeather = await axios.get(
+          `/forecast/daily?lat=${lat}&lon=${lon}&lang=${lang}&appid=${API_KEY}`
+        );
+        return dailyWeather.data;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
